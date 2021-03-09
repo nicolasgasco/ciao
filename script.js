eb50724ea660dc6ff53cc2959729cfe78b0da2e0
nicolasgasco <nicolas.gasco92@gmail.com>
2021-03-09 19:31:27 +0100

    Added screen in case nothing is found

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let apiKey = `019e3db391209165d704763866329bb3`;
 
+let searchButton = document.querySelector("#search-button");
 let searchBarTitle = document.querySelector("#search-bar");
 
-searchBarTitle.onchange = fetchMediaFromKeywords;
+searchBarTitle.addEventListener("change", fetchMediaFromKeywords);
+searchButton.addEventListener("click", fetchMediaFromKeywords);
 
-let newArray = [];
 
 function fetchMediaFromKeywords(e) {
-    let language = `en-US`
-    let query = e.target.value;
+    let language = `en-US`;
+    let query = searchBarTitle.value;
+    console.log(query)
 
 
     let link = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=${language}&query=${query}&page=1&include_adult=false`;
@@ -29,59 +31,70 @@ function createCardsWithMedia(dataSet) {
 
     resultsBox.innerHTML = ``;
 
-    let objectsArray = [];
-    for ( media of dataSet.results) {
+    if ( dataSet.results.length === 0 ) {
+        resultsBox.style.flexDirection = "column";
+        resultsBox.style.alignItems = "center";
         
-        if ( media.title ) {
-
-            let mediaData = new Object()
-            
-
-            mediaData.title =  media.title;
-            mediaData.id = media.id;
-            mediaData.img = media.poster_path;
-            mediaData.overview = media.overview;
-            mediaData.release_date = media.release_date;
-            // mediaData.popularity = media.popularity;
-            mediaData.genre_ids = media.genre_ids;
-            mediaData.vote_average = media.vote_average;
-            mediaData.vote_count = media.vote_count;
-            
-            let singleMedia = [media.vote_count, mediaData];
-            objectsArray.push(singleMedia);
-        }
-
-        
-    }
-    let = objectsArraySorted = objectsArray.sort(function(a, b) {return b[0] - a[0];});
-    
-    objectsArraySorted.forEach( element => {
-        
-        console.log(element)
-
-        if ( element[1].img ) {
-            posterUrl = `https://image.tmdb.org/t/p/w${posterSize}/${element[1].img}`;
-        } else {
-            console.log("ciao")
-            posterUrl = `./img/image_unavailable.png`;
-        }
-
-
-        resultsBox.innerHTML +=
+        resultsBox.innerHTML =
         `
-        <div class="media-card media-card-front">
+        <h2>Oops! It looks like you know more than us on cinema...</h2>
+        <small>(Either that or the media you're looking for doesn't exist.)</small>
+        <img src="./img/nothing_found.png" alt="Illustration for nothing found">
+        `
 
-            <img src="${posterUrl}" alt="Poster picture of ${element[1].title}" class="poster-pic">
-            <div class="media-card-text">
-                <h3>${element[1].title} (${element[1]["release_date"].substring(0,4)})</h3>
-                <p>Genres: ${element[1].genre_ids}</p>            
-                <p>Vote: ${element[1].vote_average}</p>
-                <p>Vote count: ${element[1].vote_count}</p>
+    } else {
+        resultsBox.style.flexDirection = "row";
+        resultsBox.style.alignItems = "baseline";
+
+        let objectsArray = [];
+        for ( media of dataSet.results) {
+            
+            if ( media.title ) {
+
+                let mediaData = new Object()
+                
+
+                mediaData.title =  media.title;
+                mediaData.id = media.id;
+                mediaData.img = media.poster_path;
+                mediaData.overview = media.overview;
+                mediaData.release_date = media.release_date;
+                // mediaData.popularity = media.popularity;
+                mediaData.genre_ids = media.genre_ids;
+                mediaData.vote_average = media.vote_average;
+                mediaData.vote_count = media.vote_count;
+                
+                let singleMedia = [media.vote_count, mediaData];
+                objectsArray.push(singleMedia);
+            }
+
+            
+        }
+        let = objectsArraySorted = objectsArray.sort(function(a, b) {return b[0] - a[0];});
+        
+        objectsArraySorted.forEach( element => {
+
+            if ( element[1].img ) {
+                posterUrl = `https://image.tmdb.org/t/p/w${posterSize}/${element[1].img}`;
+            } else {
+                posterUrl = `./img/image_unavailable.png`;
+            }
+
+
+            resultsBox.innerHTML +=
+            `
+            <div class="media-card">
+
+                <img src="${posterUrl}" alt="Poster picture of ${element[1].title}" class="poster-pic">
+                <div class="media-card-text">
+                    <h3>${element[1].title} (${element[1]["release_date"].substring(0,4)})</h3>
+                    <p>Genres: ${element[1].genre_ids}</p>            
+                    <p>Vote: ${element[1].vote_average}</p>
+                    <p>Vote count: ${element[1].vote_count}</p>
+                </div>
             </div>
-        </div>
-
-
-        `
-    })
+            `
+        })
+    }
 }
 
