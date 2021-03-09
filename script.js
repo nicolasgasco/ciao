8736ec3b7f8738300ef0a56b1f1dedb89f3f6ca4
nicolasgasco <nicolas.gasco92@gmail.com>
2021-03-10 00:12:30 +0100

    Still working on showing descriptions

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let apiKey = `019e3db391209165d704763866329bb3`;
+const apiKey = `019e3db391209165d704763866329bb3`;
+const language = `en-US`;
 
-let searchButton = document.querySelector("#search-button");
-let searchBarTitle = document.querySelector("#search-bar");
+let genresArray = createObjectMovieGenres();
 
-searchBarTitle.addEventListener("change", fetchMediaFromKeywords);
+
+const searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", fetchMediaFromKeywords);
+
+const searchBarTitle = document.querySelector("#search-bar");
+searchBarTitle.addEventListener("change", fetchMediaFromKeywords);
+
+
+
+
 
 
 function fetchMediaFromKeywords(e) {
-    let language = `en-US`;
+
     let query = searchBarTitle.value;
-    console.log(query)
 
 
     let link = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=${language}&query=${query}&page=1&include_adult=false`;
@@ -60,12 +67,18 @@ function createCardsWithMedia(dataSet) {
                 mediaData.overview = media.overview;
                 mediaData.release_date = media.release_date;
                 // mediaData.popularity = media.popularity;
-                mediaData.genre_ids = media.genre_ids;
+
+                mediaData.genre_labels = [];
+                for ( let genreId of media.genre_ids ) {
+                    mediaData.genre_labels.push(getGenreLabelFromId(genreId).toLowerCase())
+                }
+
                 mediaData.vote_average = media.vote_average;
                 mediaData.vote_count = media.vote_count;
                 
                 let singleMedia = [media.vote_count, mediaData];
                 objectsArray.push(singleMedia);
+
             }
 
             
@@ -80,21 +93,68 @@ function createCardsWithMedia(dataSet) {
                 posterUrl = `./img/image_unavailable.png`;
             }
 
-
+            const description = element[1].overview;
             resultsBox.innerHTML +=
-            `
-            <div class="media-card">
-
-                <img src="${posterUrl}" alt="Poster picture of ${element[1].title}" class="poster-pic">
-                <div class="media-card-text">
-                    <h3>${element[1].title} (${element[1]["release_date"].substring(0,4)})</h3>
-                    <p>Genres: ${element[1].genre_ids}</p>            
-                    <p>Vote: ${element[1].vote_average}</p>
-                    <p>Vote count: ${element[1].vote_count}</p>
+                `
+                <div class="media-card" id="${element[1].id}" onclick="showBacksideCard(event, ${element[1].id})">
+                    <img src="${posterUrl}" alt="Poster picture of ${element[1].title}" class="poster-pic">
+                    <p class="hidden-text" id="hidden-text-${element[1].id}">${description}</p>
+                    <div class="media-card-text" id="media-card-text-${element[1].id}">
+                        <h3>${element[1].title} (${element[1]["release_date"].substring(0,4)})</h3>
+                        <p>Genres: ${element[1].genre_labels.join(", ")}.</p>            
+                        <p>Vote: ${element[1].vote_average}</p>
+                        <p>Vote count: ${element[1].vote_count}</p>
+                    </div>
                 </div>
-            </div>
+ 
             `
-        })
+
+        });
+        
     }
 }
+
+function showBacksideCard(e, id) {
+    const mediaCard = e.currentTarget;
+    const moviePoster = e.target;
+    const hiddenText = document.querySelector(`#hidden-text-${id}`);
+    const shownText = document.querySelector(`#media-card-text-${id}`);
+
+    shownText.style.display = "none";
+    hiddenText.style.display = "block";
+
+    // hiddenText.addEventListener("click", function () {
+    //     hiddenText.setAttribute("style", "display: none;");
+
+
+    //     shownText.style.display = "block";
+    // });
+
+}
+
+
+function createObjectMovieGenres() {
+    let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=${language}`
+    let genresArray = []
+    
+    fetch(url)
+    .then( res => res.json() )
+    .then( data => {
+        
+        data.genres.forEach( genre => genresArray.push( {"id": genre.id, "name": genre.name} ));
+
+        
+    })
+    return genresArray;
+}
+
+
+function getGenreLabelFromId(id) {
+    for ( let index in genresArray ) {
+        if ( genresArray[index]["id"] === id ) {
+            return genresArray[index]["name"];
+        }
+    }
+}
+
 
